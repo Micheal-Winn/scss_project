76e815d9d6d5fc947865cd14c209a2b88e0ae781
thantzinwin <thant.zin.winnnn@gmail.com>
2023-11-11 12:48:05 +0630

    feat : add hero video

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,7 +3,24 @@ import styles from "./hero.module.scss"
 
 const Hero = () => {
   return (
-    <section className={styles.hero}>Hero</section>
+    <section className={styles.hero}>
+      <div className={styles.video__wrapper}>
+        <div>
+          <div className={styles.overlay}>
+          <video
+            autoPlay
+            muted
+            loop
+          >
+            <source
+              src="https://www.apple.com/105/media/us/apple-tv-plus/2022/755f2d0d-659f-44e2-a345-57305fabfdfa/supercut/large.mp4"
+              type="video/mp4"
+            />
+          </video>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
