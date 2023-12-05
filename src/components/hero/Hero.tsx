2ef65bed94f71681e7a3d6161f79f34ffb1faf8c
Micheal-Winn <thant.zin.winnnn@gmail.com>
2023-12-05 17:49:42 +0630

    add docker compose and dockerfile

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from "./hero.module.scss"
+import React from "react";
+import styles from "./hero.module.scss";
 
 const Hero = () => {
   return (
@@ -7,21 +7,20 @@ const Hero = () => {
       <div className={styles.video__wrapper}>
         <div>
           <div className={styles.overlay}>
-          <video
-            autoPlay
-            muted
-            loop
-          >
-            <source
-              src="https://www.apple.com/105/media/us/apple-tv-plus/2022/755f2d0d-659f-44e2-a345-57305fabfdfa/supercut/large.mp4"
-              type="video/mp4"
-            />
-          </video>
+            <video autoPlay muted loop>
+              <source
+                src="https://www.apple.com/105/media/us/apple-tv-plus/2022/755f2d0d-659f-44e2-a345-57305fabfdfa/supercut/large.mp4"
+                type="video/mp4"
+              />
+            </video>
           </div>
         </div>
       </div>
+      <div className={styles.content}>
+        <h1 className={styles.hero_text}>All movies</h1>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
